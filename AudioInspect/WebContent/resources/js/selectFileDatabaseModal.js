4a23bac16cb51bf37c5641d48926cb7fe076f558
ga0nii <kgw0124@kakao.com>
2023-01-23 15:01:25 +0900

    [23.01.23]
    TEXT, TREE, XML일 때 출력되는 차이점 구문 디자인 수정

--- AudioInspect/WebContent/resources/js/selectFileDatabaseModal.js
+++ AudioInspect/WebContent/resources/js/selectFileDatabaseModal.js
@@ -126,9 +126,18 @@ function modal_view() {
 	window.addEventListener('click', (e) => {
 		e.target === selectFileDatabase ? document.body.removeChild(selectFileDatabase) : false
 	})
+	
+	var initQueryForOriginal = "select sf.file_name, sf.recording_mode, sf.recording_quality, sf.file_type, sd.smart_device_model_name, sd.smart_device_model_number, osd.os_name, osd.os_version\n"
+		+ "from original_speech_file sf, recording_editing_device red, smart_device sd, os_for_smart_devices osd\n"
+		+ "where sf.recording_device_id=red.recording_editing_device_id and red.smart_device_id = sd.smart_device_id and red.os_id = osd.os_id;"
+	var initQueryForEdited = "select esf.file_name, esf.editing_app_name, esf.recording_mode, esf.recording_quality, esf.file_type, sd.smart_device_model_name, sd.smart_device_model_number, osd.os_name, osd.os_version\n"
+		+ "from edited_speech_file esf, recording_editing_device red, smart_device sd, os_for_smart_devices osd\n"
+		+ "where esf.editing_device_id=red.recording_editing_device_id and red.smart_device_id = sd.smart_device_id and red.os_id = osd.os_id;"
 
 	// html dom 이 다 로딩된 후 실행된다.
 	$(document).ready(function() {
+		getFileListFromDB(initQueryForOriginal)
+		getFileListFromDB(initQueryForEdited)
 		// menu 클래스 바로 하위에 있는 a 태그를 클릭했을때
 		$(".menu>a").click(function() {
 			var submenu = $(this).next("ul")
@@ -150,13 +159,4 @@ function modal_view() {
 			}
 		})
 	})
-
-	var initQueryForOriginal = "select sf.file_name, sf.recording_mode, sf.recording_quality, sf.file_type, sd.smart_device_model_name, sd.smart_device_model_number, osd.os_name, osd.os_version\n"
-		+ "from original_speech_file sf, recording_editing_device red, smart_device sd, os_for_smart_devices osd\n"
-		+ "where sf.recording_device_id=red.recording_editing_device_id and red.smart_device_id = sd.smart_device_id and red.os_id = osd.os_id;"
-	var initQueryForEdited = "select esf.file_name, esf.editing_app_name, esf.recording_mode, esf.recording_quality, esf.file_type, sd.smart_device_model_name, sd.smart_device_model_number, osd.os_name, osd.os_version\n"
-		+ "from edited_speech_file esf, recording_editing_device red, smart_device sd, os_for_smart_devices osd\n"
-		+ "where esf.editing_device_id=red.recording_editing_device_id and red.smart_device_id = sd.smart_device_id and red.os_id = osd.os_id;"
-	getFileListFromDB(initQueryForOriginal)
-	getFileListFromDB(initQueryForEdited)
 }
