336fb796fdfbfa1b7609cd3b108faf3de05c61cc
Haeji-Cho <80144964+Haeji-Cho@users.noreply.github.com>
2023-02-13 17:57:28 +0900

    [23.02.13]
    주석 1차 추가

--- AudioInspect/WebContent/resources/js/getOptionFileListFromDB.js
+++ AudioInspect/WebContent/resources/js/getOptionFileListFromDB.js
@@ -52,69 +52,87 @@ function manuclick() {
 	let result2 = '';
 	let result3 = '';
 	let result4 = '';
+	//selectedEls.forEach{}내부에 있어야 체크박스 선택, 선택 취소가 원활하게 확인됨
+	//그래서 forEach문 내부에서 다중 선택 가능 배열을 넣었음
 	selectedEls.forEach((el) => {
 		result = el.name + '';
 		resultarr.push(result);
 		result2 = el.className + '';
 		resultarr_2.push(result2)
 		result3 = el.className + '';
+		//디바이스 제조사 설정은 다중 선택이 가능하므로 따로 배열 생성
 		if (result3 == "manufacturerlist") {
 			resultarr_3.push(" and sf.recording_app_manufacturer='" + result + "'")
 			resultarr_4.push(" and sf.recording_app_manufacturer='" + result + "'")
 		}
+		//편집 소프트웨어 설정은 다중 선택이 가능하므로 따로 배열 생성
 		result4 = el.className + '';
 		if (result4 == "editlist2") {
 			resultarr_5.push(" and esf.editing_app_name like'%" + result + "%'")
 		}
 	});
 	for (var i = 0; i < resultarr.length; i++) {
+		//manufacturerlist = 디바이스 제조사 설정
 		if (resultarr_2[i] == "manufacturerlist") {
 			if (resultarr[i] == "manuAll") {
+				//전체 선택 클릭한 경우 나머지 디바이스 제조사 설정 체크 취소
 				$("input[id = 'nonck']").prop("checked", false);
 			} else {
+				//전체 선택 이외의 디바이스 제조사 설정 클릭시 전체 선택 체크 취소
 				if (resultarr[i] == "manuAll") {
 					resultarr.splice(0)
 				}
+				$("input[name = 'manuAll']").prop("checked", false);
+				//디바이스 제조사 설정 클릭시 query문 배열 내부에 추가
 				selectmanufacturer[i] = resultarr_3[i]
 				selectmanufacturer2[i] = resultarr_4[i]
-				$("input[name = 'manuAll']").prop("checked", false);
 			}
 		}
+		//편집/원본 여부 선택
 		if (resultarr_2[i] == "editlist") {
 			if (resultarr[i] == "selectAll") {
+				//전체 클릭시 나머지 체크 취소
 				$("input[id = 'editnonck']").prop("checked", false);
 			}
 			if (resultarr[i] == "selectorigin") {
+				//원본만 보기 클릭시 전체 선택, 편집본만 보기 선택 취소
 				$("input[name='selectAll']").prop("checked", false);
 				$("input[name='edit']").prop("checked", false);
+				//원본만 보기 클릭하므로 편집 소프트웨어 선택 사항들 선택 취소
 				const checkbox5 = $(".editlist2");
 				for (var j = 0; j < checkbox5.length; j++) {
 					checkbox5[j].checked = false;
 				}
 			}
 			if (resultarr[i] == "edit") {
+				//편집만 보기 선택시 전체 보기 선택, 원본만 보기 선택 취소
 				$("input[name='selectAll']").prop("checked", false);
 				$("input[name='selectorigin']").prop("checked", false);
 			}
 		}
+		//편집 소프트웨어 설정
 		if (resultarr_2[i] == "editlist2") {
 			if (resultarr[i] == "selectedit") {
+				//편집 소프트웨어 설정 내부의 전체보기 선택
+				//editlist의 전체 보기, 원본만 보기 선택 취소
 				$("input[name='selectAll']").prop("checked", false);
 				$("input[name='selectorigin']").prop("checked", false);
 				$("input[id = 'sweditnonck']").prop("checked", false);
 			}
 			else {
+				//편집 소프트웨어 선택
+				//eidtlist의 전체 보기, 원본만 보기 선택 취소
 				$("input[name='edit']").prop("checked", true);
 				$("input[name='selectAll']").prop("checked", false);
 				$("input[name='selectorigin']").prop("checked", false);
 				$(".result_list").empty();
 				$("input[name='selectedit']").prop("checked", false);
-				//selectEdit2[i - selectmanufacturer2.length] = resultarr_5[i]
-				//resultarr_5[i] = resultarr_5[i]
 			}
 		}
+		//OS 설정
 		if (resultarr_2[i] == "OSlist") {
 			if (resultarr[i] == "OSAll") {
+				//전체 보기 선택시 OS 버전 선택, 직접 입력 선택 취소
 				if ($(".input_OS").is(":checked") == true) {
 					$("input[class='input_OS']").prop("checked", false);
 				}
@@ -123,48 +141,65 @@ function manuclick() {
 				}
 				$("input[id='OSnonck']").prop("checked", false);
 			} else {
+				//전체 보기 이외에 Android, iOS 선택시 전체 보기 선택 취소
 				$("input[name='OSAll']").prop("checked", false);
+				//Android, iOS 선택시 query생성
+				//resultarr[i]의 값이 Android이거나 iOS임
 				selectOS = " and osd.os_name='" + resultarr[i] + "'";
 				selectOS2 = " and osd.os_name='" + resultarr[i] + "'";
 			}
 		}
+		//검색 키워드 설정
 		if (resultarr_2[i] == "text_search") {
+			//파일 이름으로 검색
 			if (resultarr[i] == "file_name") {
+				//파일 이름으로 검색 선택시 녹음 모델명, 녹음 모델 넘버 선택 취소
 				$("input[name='record_device']").prop("checked", false);
 				$("input[name='record_device_num']").prop("checked", false);
+				//$(".search_text").val()이 사용자가 입력하는 값
 				var search = $(".search_text").val();
 				search_file_name = " and sf.file_name like'%" + search + "%'";
 				search_file_name2 = " and esf.file_name like'%" + search + "%'";
+				//파일 이름, 녹음 모델명, 모델 넘버를 한 칸으로 검색하므로 파일 이름 검색시 이전에 검색했던 내용 삭제
 				search_model_name = "";
 				search_model_name2 = "";
 				search_model_number = "";
 				search_model_number2 = "";
 
 			}
+			//녹음 모델명으로 검색
 			if (resultarr[i] == "record_device") {
+				//녹음 모델명 검색 선택시 파일 이름, 녹음 모델 넘버 선택 취소
 				$("input[name='file_name']").prop("checked", false);
 				$("input[name='record_device_num']").prop("checked", false);
+				//$(".search_text").val()이 사용자가 입력하는 값
 				var search2 = $(".search_text").val();
 				search_model_name = " and sd.smart_device_model_name like'%" + search2 + "%'";
 				search_model_name2 = " and sd.smart_device_model_name like'%" + search2 + "%'";
+				//파일 이름, 녹음 모델명, 모델 넘버를 한 칸으로 검색하므로 파일 이름 검색시 이전에 검색했던 내용 삭제
 				search_file_name = "";
 				search_file_name2 = "";
 				search_model_number = "";
 				search_model_number2 = "";
 
 			}
+			//녹음 모델 넘버로 검색
 			if (resultarr[i] == "record_device_num") {
+				//녹음 모델 넘버로 검색 선택시 파일 이름, 녹음 모델명 선택 취소
 				$("input[name='file_name']").prop("checked", false);
 				$("input[name='record_device']").prop("checked", false);
+				//$(".search_text").val()이 사용자가 입력하는 값
 				var search3 = $(".search_text").val();
 				search_model_number = " and sd.smart_device_model_number like'%" + search3 + "%'";
 				search_model_number2 = " and sd.smart_device_model_number like'%" + search3 + "%'";
+				//파일 이름, 녹음 모델명, 모델 넘버를 한 칸으로 검색하므로 파일 이름 검색시 이전에 검색했던 내용 삭제
 				search_file_name = "";
 				search_file_name2 = "";
 				search_model_name = "";
 				search_model_name2 = "";
 			}
 		}
+		
 		var origin = [];
 		var edited = [];
 		if (resultarr_2[i].includes("edit")) {
@@ -269,51 +304,67 @@ function manuclick() {
 	}
 }
 
+//OS 설정 내부에서 OS 버전 선택
 function OS_number() {
-	if ($(".input_OS").is(":checked") == true) {
-		$("input[class='input_OS']").prop("checked", false);
-	}
+	//OS 버전 선택 드롭 다운 선택지에서 한 가지 선택시 OS 버전 선택 체크 박스 체크
 	if ($(".select_OS").is(":checked") == false) {
 		$("input[class='select_OS']").prop("checked", true);
+	}
+	//직접 입력, 전체 보기 체크 박스 체크 취소
+	if ($(".input_OS").is(":checked") == true) {
+		$("input[class='input_OS']").prop("checked", false);
 	}
 	if ($("input[name='OSAll']").is(":checked") == true) {
 		$("input[name='OSAll']").prop("checked", false);
 	}
+	//$(".selectOS").val()는 OS 버전 선택에서 고른 선택 값
 	var OS_version = $(".selectOS").val()
+	//$("select[class = selectOS] option:selected").text()는 Android인지 iOS인지 구별
 	var OS_And_iOS = $("select[class = selectOS] option:selected").text();
+	//버전이 Android인 경우 OSlist의 Android 체크 
 	if (OS_And_iOS.includes("Android")) {
 		$("input[name = 'Android']").prop("checked", true);
 		$("input[name = 'iOS']").prop("checked", false);
 	}
+	//버전이 iOS인 경우 OSlist의 iOS 체크
 	else if (OS_And_iOS.includes("iOS")) {
 		$("input[name = 'iOS']").prop("checked", true);
 		$("input[name = 'Android']").prop("checked", false);
 	}
-	select_Android3 = "";
-	select_Android4 = "";
 	select_Android = " and osd.os_version like '%" + OS_version + "' ";
 	select_Android2 = " and osd.os_version like '%" + OS_version + "'";
+	//직접 입력으로 검색한 값 삭제
+	select_Android3 = "";
+	select_Android4 = "";
+	//상단의 manuclick함수 호출
 	manuclick();
 }
+//OS 설정 내부에서 직접 입력 선택 or 입력
 function OS_number2() {
+	//직접 입력 칸에 글자 검색시 직접 입력 선택 체크
 	if ($(".input_OS").is(":checked") == false) {
 		$("input[class='input_OS']").prop("checked", true);
 	}
+	//OS버전 선택, 전체 보기 선택 체크 취소
 	if ($(".select_OS").is(":checked") == true) {
 		$("input[class='select_OS']").prop("checked", false);
 	}
 	if ($("input[name='OSAll']").is(":checked") == true) {
 		$("input[name='OSAll']").prop("checked", false);
 	}
+	//입력칸에 입력한 값
 	var OS_version2 = $(".search").val();
-	select_Android = "";
-	select_Android2 = "";
 	select_Android3 = " and osd.os_version like '%" + OS_version2 + "%'";
 	select_Android4 = " and osd.os_version like '%" + OS_version2 + "%'";
+	//OS버전 선택 값 삭제
+	select_Android = "";
+	select_Android2 = "";
+	//manuclick 함수 호출
 	manuclick();
 }
 
-
+//녹음 모드 선택
+//녹음 모드 선택에서 전체 보기 선택
 function recordclick() {
 	const query = 'input[class="recordlist"]:checked';
 	const selectedEls = document.querySelectorAll(query);
@@ -327,42 +378,61 @@ function recordclick() {
 	}
 	manuclick()
 }
+//녹음 모드 선택
+//녹음 모드 선택에서 녹음 모드, 녹음 퀄리티 선택
 function recordmode() {
+	//녹음 모드 선택에서 선택시 전체 보기 선택 취소
 	if ($("input[class='recordlist']").is(":checked") == true) {
 		$("input[class='recordlist']").prop("checked", false);
 	}
+	//녹음 모드 선택 값
 	var mode = $(".recordmode").val();
+	//녹음 퀄리티 선택 값
 	var mode2 = $(".recordQ").val();
 	$(".result_list").empty();
+	//녹음 모드 중 전체 보기 선택시 녹음 모드 선택 값 삭제
 	if (mode == "recordmode_All") {
 		select_recordmode = "";
 		select_recordmode2 = "";
-	} else {
+	}
+	//녹음 모드 query 생성
+	else {
 		select_recordmode = " and sf.recording_mode='" + mode + "'";
 		select_recordmode2 = " and esf.recording_mode='" + mode + "'";
 	}
+	//녹음 퀄리티 중 전체 보기 선택시 녹음 퀄리티 선택 값 삭제
 	if (mode2 == "recordQ_All") {
 		select_recordQ = "";
 		select_recordQ2 = "";
-	} else {
+	} 
+	//녹음 퀄리티 query 생성
+	else {
 		select_recordQ = " and sf.recording_quality='" + mode2 + "'";
 		select_recordQ2 = " and esf.recording_quality='" + mode2 + "'";
 	}
+	//녹음 모드와 녹음 퀄리티 query 합치기
 	record = select_recordmode + select_recordQ;
 	record2 = select_recordmode2 + select_recordQ2;
+	//manuclick 함수 호출
 	manuclick()
 }
 
-
+//선택 초기화 클릭
 function deleteclick() {
 	$(".result_list").empty();
 	// 초기화할 checkbox 선택
+	//디바디스 제조사
 	const checkbox = $(".manufacturerlist");
+	//OS 설정
 	const checkbox2 = $(".OSlist");
+	//워본/퍈집 설정
 	const checkbox3 = $(".editlist");
+	//검색 키워드 설정
 	const checkbox4 = $(".text_search")
+	//편집 소프트웨어 설정
 	const checkbox5 = $(".editlist2");
 	// 체크박스 목록을 순회하며 checked 값을 초기화
+	//한 선택 사항 내에 다중 선택이 될 수 있으므로 for문으로 해당 부분 체크 전체 삭제
 	for (var i = 0; i < checkbox.length; i++) {
 		checkbox[i].checked = false;
 	}
@@ -385,10 +455,15 @@ function deleteclick() {
 	}
 	$("input[class='select_OS']").prop("checked", false);
 	$("input[class='input_OS']").prop("checked", false);
+	//OS 버전 직접 입력 값 삭제
 	$(".search").val("")
+	//녹음 모드 전체보기로 변경
 	$(".recordmode").val("recordmode_All")
+	//녹음 퀄리티 전체보기로 변경
 	$(".recordQ").val("recordQ_All")
+	//검색 키워드 입력 값 삭제
 	$(".search_text").val("")
+	//배열, 저장된 값 초기 상황으로 되돌리기
 	selectEdit = "";
 	selectEdit2 = [];
 	selectOS = "";
@@ -417,6 +492,8 @@ function deleteclick() {
 	resultarr_2 = [];
 	resultarr_3 = [];
 	resultarr_4 = [];
+	//모달 창 처음 띄울 때와 같은 결과 부르기
+	//전체 보기와 같은 결과 나옴
 	getFileListFromDB(initQueryForOriginal)
 	getFileListFromDB(initQueryForEdited)
 }
